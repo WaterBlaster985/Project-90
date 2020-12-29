67fa91ca71324e309aeadb77d41408b8e62cc648
WaterBlaster985 <74526954+WaterBlaster985@users.noreply.github.com>
2020-12-29 15:24:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function nextPage(){
+    player1=document.getElementById("player1input").value
+     player2=document.getElementById("player2input").value
+     localStorage.setItem("p1", player1);
+     localStorage.setItem("p2", player2);
+     window.location="index2.html"
+}
